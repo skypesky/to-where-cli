fa5b756c2b2f05c29e27bf3a84dddf0a7c3f6b30
skypesky <2565978507@qq.com>
2022-10-23 23:09:07 +0800

    feat: update codes

--- src/protocol/control.protocol.ts
+++ src/protocol/control.protocol.ts
@@ -1,13 +1,15 @@
+import { PointMeta } from "../meta";
+
 export interface ControlProtocol {
   cd(point: string): Promise<void>;
 
-  create(point: string, path: string, force: boolean): Promise<void>;
+  create(point: string, path: string, force: boolean): Promise<PointMeta>;
   
   delete(point: string): Promise<void>;
 
-  show(point: string): Promise<string>;
+  show(point: string): Promise<PointMeta>;
 
-  list(): Promise<string>;
+  list(): Promise<PointMeta[]>;
 
   clean(): Promise<void>;
 }
