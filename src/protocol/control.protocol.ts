4d23466205b1684552b46c2e1fb010f0726b1481
skypesky <2565978507@qq.com>
2022-10-23 22:59:50 +0800

    feat: update codes

--- src/protocol/control.protocol.ts
+++ src/protocol/control.protocol.ts
@@ -1,6 +1,13 @@
 export interface ControlProtocol {
-    add(point: string, path: string, force: boolean): Promise<void>;
-    cd(point: string): Promise<void>;
-    delete(): Promise<void>;
+  cd(point: string): Promise<void>;
 
+  create(point: string, path: string, force: boolean): Promise<void>;
+  
+  delete(point: string): Promise<void>;
+
+  show(point: string): Promise<string>;
+
+  list(): Promise<string>;
+
+  clean(): Promise<void>;
 }
