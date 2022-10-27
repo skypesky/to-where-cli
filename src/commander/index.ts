48239b22869b892c8cbab317373cea810a5ecda8
skypesky <2565978507@qq.com>
2022-10-28 00:46:48 +0800

    feat: support add command string

--- src/commander/index.ts
+++ src/commander/index.ts
@@ -1,14 +1,27 @@
 import { program } from 'commander';
 import * as packageJson from '../../package.json';
+import { logger } from '../utils/logger';
 
 program.name("td").description('ABC').version(packageJson.version);
 
-program
-    .option('--first')
-    .option('-s, --separator <char>');
+program.command("<point>")
+    .description('Warps to the directory specified by the warp point')
+    .action((str, options) => {
+        logger.log({ str, options });
+    });
+
+program.command("<point> <path>")
+    .description('Warps to the directory specified by the warp point with path appended')
+    .action((str, options) => {
+        logger.log({ str, options });
+    });
+
+program.command('add')
+    .description('Adds the current working directory to your warp points')
+    .argument('<point>', 'TODO')
+    .action((str, options) => {
+        logger.log({ str, options })
+
+    });
 
 program.parse();
-
-const options = program.opts();
-const limit = options.first ? 1 : undefined;
-console.log(program.args[0].split(options.separator, limit));
