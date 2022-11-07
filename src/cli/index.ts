5b513c2e42317b27f095e341899c182b58c21db3
skypesky <2565978507@qq.com>
2022-11-08 01:09:46 +0800

    feat: update codes

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,23 +1,26 @@
-import { program } from 'commander';
-import * as packageJson from '../../package.json';
-import { logger } from '../utils/logger';
-import { addCommand } from './add';
-import { cleanCommand } from './clean';
-import { listCommand } from './list';
-import { removeCommand } from './rm';
+#! /usr/bin/env node
+
+import { program } from "commander";
+import * as packageJson from "../../package.json";
+import { logger } from "../utils/logger";
+import { addCommand } from "./add";
+import { cleanCommand } from "./clean";
+import { listCommand } from "./list";
+import { removeCommand } from "./rm";
 
 // @see: https://www.npmjs.com/package/commander
 
-program.name("td").description('ABC').version(packageJson.version);
+program.name("td").description("ABC").version(packageJson.version);
 
-program.command("<point>")
-    .description('Warps to the directory specified by the warp point')
-    .action((str, options) => {
-        logger.log({ str, options });
-    });
+program
+  .command("<point>")
+  .description("Warps to the directory specified by the warp point")
+  .action((str, options) => {
+    logger.log({ str, options });
+  });
 
 program.addCommand(addCommand);
-program.addCommand(removeCommand)
+program.addCommand(removeCommand);
 program.addCommand(listCommand);
 program.addCommand(cleanCommand);
 
