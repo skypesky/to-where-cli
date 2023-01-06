b54e698e19fa5998713edbb8c8e1bd3d509166a9
skypesky <2565978507@qq.com>
2023-01-07 00:59:34 +0800

    feat: support open more git pages

--- tests/cli/git.spec.ts
+++ tests/cli/git.spec.ts
@@ -1,90 +1,122 @@
 import { basename } from "path";
 import { createProgram } from "../../src";
-import open from 'open';
+import open from "open";
 import gitRemoteOriginUrl from "git-remote-origin-url";
-import getRepoInfo, {GitRepoInfo} from 'git-repo-info'
-import urlJoin  from 'url-join';
+import getRepoInfo, { GitRepoInfo } from "git-repo-info";
+import urlJoin from "url-join";
 
-jest.mock('open')
+jest.mock("open");
 const openMock = jest.mocked(open);
 
-
-jest.mock('git-remote-origin-url');
+jest.mock("git-remote-origin-url");
 const gitRemoteOriginUrlMock = jest.mocked(gitRemoteOriginUrl);
 
-jest.mock('git-repo-info');
+jest.mock("git-repo-info");
 const getRepoInfoMock = jest.mocked(getRepoInfo);
 
 describe(basename(__filename), () => {
-
-  const githubAddress = 'https://github.com/skypesky/to-where-cli';
+  const errorSpy = jest.spyOn(console, "error").mockReturnThis();
+  const githubAddress = "https://github.com/skypesky/to-where-cli";
   const gitRepoInfo: GitRepoInfo = {
-    branch: 'master',
-    sha: "",
+    branch: "master",
+    sha: "xxx",
     abbreviatedSha: "",
     tag: "",
     lastTag: "",
     commitsSinceLastTag: 0,
-    committer: "",
+    committer: "skypesky",
     committerDate: "",
     author: "skypesky",
     authorDate: "",
     commitMessage: "",
     root: "",
     commonGitDir: "",
-    worktreeGitDir: ""
-  }
+    worktreeGitDir: "",
+  };
 
   beforeEach(() => {
     openMock.mockReturnThis();
     gitRemoteOriginUrlMock.mockResolvedValue(githubAddress);
     getRepoInfoMock.mockReturnValue(gitRepoInfo);
   });
-  
+
   afterEach(() => {
     jest.resetAllMocks();
     jest.clearAllMocks();
     jest.restoreAllMocks();
-  })
+  });
+
+  it("should be throw an error when current directory is not a valid git repository", async () => {
+    gitRemoteOriginUrlMock.mockImplementation(() => {
+      throw new Error("current directory is not a valid git repository");
+    });
+
+    const program = createProgram();
+
+    await program.parseAsync(["ts-node", "index.ts", "git", "open"]);
+
+    expect(errorSpy).toHaveBeenCalledWith(
+      `The current directory is not a valid git repository`
+    );
+  });
 
   it("should be work when call tw git open", async () => {
     const program = createProgram();
 
-    await program.parseAsync(["ts-node", "index.ts", "git", 'open']);
+    await program.parseAsync(["ts-node", "index.ts", "git", "open"]);
 
-    expect(openMock).toBeCalledWith(urlJoin(githubAddress, 'tree', gitRepoInfo.branch));
+    expect(openMock).toBeCalledWith(
+      urlJoin(githubAddress, "tree", gitRepoInfo.branch)
+    );
   });
 
   it("should be work when call tw git open -a", async () => {
     const program = createProgram();
 
-    await program.parseAsync(["ts-node", "index.ts", "git", 'open', '-a']);
+    await program.parseAsync(["ts-node", "index.ts", "git", "open", "-a"]);
 
-    expect(openMock).toBeCalledWith(urlJoin(githubAddress, 'actions'));
+    expect(openMock).toBeCalledWith(urlJoin(githubAddress, "actions"));
   });
 
   it("should be work when call tw git open -i", async () => {
     const program = createProgram();
 
-    await program.parseAsync(["ts-node", "index.ts", "git", 'open', '-i']);
+    await program.parseAsync(["ts-node", "index.ts", "git", "open", "-i"]);
 
-    expect(openMock).toBeCalledWith(urlJoin(githubAddress, 'issues'));
+    expect(openMock).toBeCalledWith(urlJoin(githubAddress, "issues"));
   });
 
   it("should be work when call tw git open -p", async () => {
     const program = createProgram();
 
-    await program.parseAsync(["ts-node", "index.ts", "git", 'open', '-p']);
+    await program.parseAsync(["ts-node", "index.ts", "git", "open", "-p"]);
 
-    expect(openMock).toBeCalledWith(urlJoin(githubAddress, 'pulls'));
+    expect(openMock).toBeCalledWith(urlJoin(githubAddress, "pulls"));
   });
 
-  it("should be work when call tw git open -p", async () => {
+  it("should be work when call tw git open -r", async () => {
     const program = createProgram();
 
-    await program.parseAsync(["ts-node", "index.ts", "git", 'open', '--author']);
+    await program.parseAsync(["ts-node", "index.ts", "git", "open", "-r"]);
 
-    // FIXME: 多个options的选项值同时出现
-    expect(openMock).toBeCalledWith(urlJoin(githubAddress));
+    expect(openMock).toBeCalledWith(urlJoin(githubAddress, "releases"));
+  });
+
+  it("should be work when call tw git open -s", async () => {
+    const program = createProgram();
+
+    await program.parseAsync(["ts-node", "index.ts", "git", "open", "-s"]);
+
+    expect(openMock).toBeCalledWith(urlJoin(githubAddress, "settings"));
+  });
+
+  it("should be work when call tw git open --sha", async () => {
+    const program = createProgram();
+
+    await program.parseAsync(["ts-node", "index.ts", "git", "open", "--sha"]);
+
+    expect(openMock).toBeCalledWith(
+      urlJoin(githubAddress, "commit", gitRepoInfo.sha)
+    );
   });
 });
