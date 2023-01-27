8c6552580e57d08e06d2505713716ecd4a86bffc
skypesky <2565978507@qq.com>
2023-01-27 11:22:53 +0800

    fix: some bug

--- src/classes/open.ts
+++ src/classes/open.ts
@@ -1,10 +1,10 @@
 import { ChildProcess } from "child_process";
-import currnetOS from "current-os";
+import currentOS from "current-os";
 import { default as defaultOpen, Options } from "open";
 import exec from "shelljs.exec";
 
 export function open(target: string, options?: Options): Promise<ChildProcess> {
-  if (currnetOS.isWindows) {
+  if (currentOS.isWindows) {
     // @see https://www.npmjs.com/package/shelljs.exec
     exec(`start ${target}`);
     return null;
