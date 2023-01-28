70456a1d28cf5a3ea499b73804e7f0fa543284b8
skypesky <2565978507@qq.com>
2023-01-28 14:53:41 +0800

    tests: ci v8

--- test.js
+++ test.js
@@ -5,22 +5,13 @@ const { readFileSync } = require("fs-extra");
 const dayjs = require("dayjs");
 const { join } = require("path");
 
-(async () => {
-  try {
-    const currentVersion = readFileSync(join(cwd(), "version"))
-      .toString()
-      .trim();
-    const time = dayjs().format("YYYY-MM-DD-HH-MM-SSS");
+const currentVersion = readFileSync(join(cwd(), "version")).toString().trim();
+const time = dayjs().format("YYYY-MM-DD-HH-MM-SSS");
 
-    const newVersion = `${currentVersion}-beta-${time}`;
-    const workspace = new WorkSpaces({
-      rootDir: cwd(),
-      workSpaces: ["core"],
-      includeWorkspaceRoot: true,
-    });
-    await workspace.setVersion(newVersion);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-})();
+const newVersion = `${currentVersion}-beta-${time}`;
+const workspace = new WorkSpaces({
+  rootDir: cwd(),
+  workSpaces: ["core"],
+  includeWorkspaceRoot: true,
+});
+workspace.setVersion(newVersion);
