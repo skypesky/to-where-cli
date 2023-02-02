31eb10617dc52df19caec4bea9be5337013ccf11
skypesky <2565978507@qq.com>
2023-02-03 01:09:52 +0800

    feat: support using  to create pr

--- src/cli/git.ts
+++ src/cli/git.ts
@@ -24,13 +24,18 @@ gitCommand
   .description("Open github repo page, issues page, pr page, ...etc")
   .option("-a, --actions", "Open actions page", false)
   .option("--author", "Open author profile page", false)
-  .option("-c, --commit [commitId]", "Open commit page")
+  .option("-c, --commit [hash]", "Open commit page")
   .option("--committer", "Open committer profile page", false)
   .option("-f, --file <filePath>", "Open specific file page")
   .option("--find", "Open the search file page", false)
   .option("-i, --issue", "Open issues list page", false)
   .option("-m, --main", "Open main branch page", false)
   .option("-p, --pull-request", "Open pull request list page", false)
+  .option(
+    "--pull [branch]",
+    "Open the page for creating a pull request, the branch defaults to the current branch",
+    false
+  )
   .option("-r, --release", "Open release page", false)
   .option("-s, --settings", "Open settings page", false)
   .action(async (options: ActionOptions) => {
@@ -42,6 +47,7 @@ gitCommand
     const find = <boolean>options.find;
     const issue = <boolean>options.issue;
     const pullRequest = <boolean>options.pullRequest;
+    const pull = <string>options.pull;
     const release = <boolean>options.release;
     const main = <boolean>options.main;
     const settings = <boolean>options.settings;
@@ -57,45 +63,61 @@ gitCommand
     if (actions) {
       addresses.push(urlJoin(githubAddress, "actions"));
     }
+
     if (issue) {
       addresses.push(urlJoin(githubAddress, "issues"));
     }
+
     if (pullRequest) {
       addresses.push(urlJoin(githubAddress, "pulls"));
     }
+
+    if (pull) {
+      const info = getRepoInfo();
+      const branchName: string = isBoolean(pull) ? info.branch : pull;
+      addresses.push(urlJoin(githubAddress, "pull/new", branchName));
+    }
+
     if (author) {
       const info = getRepoInfo();
       const [$author] = info.author.split(" ");
       addresses.push(urlJoin(new URL(githubAddress).origin, $author));
     }
+
     if (release) {
       addresses.push(urlJoin(githubAddress, "releases"));
     }
+
     if (commit) {
       const info = getRepoInfo();
       const $commit: string = isBoolean(commit) ? info.sha : commit;
       addresses.push(urlJoin(githubAddress, "commit", $commit));
     }
+
     if (committer) {
       const info = getRepoInfo();
       const [$committer] = info.author.split(" ");
       addresses.push(urlJoin(new URL(githubAddress).origin, $committer));
     }
+
     if (file) {
       const info = getRepoInfo();
       // FIXME: 需要默认跳转到主分支
       const branchName = info.branch ?? "";
       addresses.push(urlJoin(githubAddress, "tree", branchName, file));
     }
+
     if (find) {
       const info = getRepoInfo();
       // FIXME: 需要默认跳转到主分支
       const branchName = info.branch ?? "";
       addresses.push(urlJoin(githubAddress, "find", branchName));
     }
+
     if (settings) {
       addresses.push(urlJoin(githubAddress, "settings"));
     }
+
     if (main) {
       // 什么都不用做
       addresses.push(githubAddress);
