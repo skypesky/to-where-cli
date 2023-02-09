7a8df965637d82931ed06ba082ff6f233f373b98
skypesky <2565978507@qq.com>
2023-02-10 00:22:34 +0800

    feat: migrate npm search v2

--- src/cli/npm/search.ts
+++ src/cli/npm/search.ts
@@ -1,6 +1,5 @@
 import { Command } from "commander";
 import { open } from "../../classes";
-import { ActionOptions } from "../../meta/actions-options";
 
 const npmSearchCommand = new Command();
 
@@ -8,9 +7,7 @@ npmSearchCommand
   .name("search")
   .argument("<keyword>", "search npm packages")
   .description("TODO")
-  .action(async (keyword: string, options: ActionOptions) => {
-    console.log({ keyword, options });
-
+  .action(async (keyword: string) => {
     const searchUrl = `https://www.npmjs.com/search?q=${keyword}`;
 
     await open(searchUrl);
