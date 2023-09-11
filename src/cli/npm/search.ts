b85293634d1a49316b71fc2d2276b307d7084306
skypesky <2565978507@qq.com>
2023-09-11 22:46:37 +0800

    fix: test

--- src/cli/npm/search.ts
+++ src/cli/npm/search.ts
@@ -1,4 +1,4 @@
-import { Command } from "commander";
+import { Command, Option } from "commander";
 import { open } from "../../classes";
 
 const npmSearchCommand = new Command();
@@ -6,11 +6,21 @@ const npmSearchCommand = new Command();
 npmSearchCommand
   .name("search")
   .argument("[keyword]", "Search by keyword")
+  .option('-v,--version', 'Search by version')
   .description("Search by keyword")
-  .action(async (keyword = "") => {
-    const searchUrl = `https://www.npmjs.com/search?q=${keyword}`;
+  .action(async (keyword: string, options: { version: boolean } ) => {
 
-    await open(searchUrl);
+    let pageUrl: string = null;
+
+    if(keyword && options.version) {
+      pageUrl = `https://www.npmjs.com/package/${keyword}?activeTab=versions`;
+    } else if(keyword) {
+      pageUrl = `https://www.npmjs.com/search?q=${keyword}`;
+    } else {
+      pageUrl = `https://www.npmjs.com`;
+    }
+
+    await open(pageUrl);
   });
 
 export { npmSearchCommand };
--- tests/cli/npm/search.spec.ts
+++ tests/cli/npm/search.spec.ts
@@ -6,7 +6,7 @@ jest.mock("../../../src/classes/open");
 const openMock = jest.mocked(open);
 
 describe(basename(__filename), () => {
-  const npmWebsite = `https://www.npmjs.com/search?q=`;
+  const npmWebsite = `https://www.npmjs.com`;
 
   it("the npm page should be opened when key is not set", async () => {
     const program = createProgram();
@@ -21,6 +21,6 @@ describe(basename(__filename), () => {
 
     await program.parseAsync(["ts-node", "index.ts", "npm", "to-where-cli"]);
 
-    expect(openMock).toHaveBeenCalledWith(`${npmWebsite}to-where-cli`);
+    expect(openMock).toHaveBeenCalledWith(`${npmWebsite}/search?q=to-where-cli`);
   });
 });
