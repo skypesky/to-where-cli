f33aa7778b9e0338524a60fcadea8e4fa4214d66
skypesky <2565978507@qq.com>
2024-06-06 23:38:44 +0800

    feat: add run kit to npm

--- src/cli/npm/search.ts
+++ src/cli/npm/search.ts
@@ -1,37 +1,50 @@
-import { Command } from "commander";
-import { open } from "../../classes";
+import { Command } from 'commander';
+import { open } from '../../classes';
 
 function npmSearchCommand(): Command {
   const command = new Command();
 
   command
-    .name("search")
-    .argument("[keyword]", "Search by keyword")
+    .name('search')
+    .argument('[keyword]', 'Search by keyword')
     .option('-c,--code', 'Open package code page')
     .option('-d,--dependencies', 'Open package dependencies page')
     .option('-v,--version', 'Open package versions page')
-    .description("Search by keyword")
-    .action(async (keyword: string, options: { version: boolean, code: boolean, dependencies: boolean } ) => {
-  
-      let pageUrl: string = null;
-  
-      if(keyword && options.version) {
-        pageUrl = `https://www.npmjs.com/package/${keyword}?activeTab=versions`;
-      } else if(keyword && options.code) {
-        pageUrl = `https://www.npmjs.com/package/${keyword}?activeTab=code`;
-      } else if(keyword && options.dependencies) {
-        pageUrl = `https://www.npmjs.com/package/${keyword}?activeTab=dependencies`;
-      } else if(keyword) {
-        pageUrl = `https://www.npmjs.com/search?q=${keyword}`;
-      } else {
-        pageUrl = `https://www.npmjs.com`;
-      }
-  
-      await open(pageUrl);
-    });
-  
-    return command;
-}
+    .option('-r,--run-kit', 'Open run kit page')
+    .description('Search by keyword')
+    .action(
+      async (
+        keyword: string,
+        options: {
+          version: boolean;
+          code: boolean;
+          dependencies: boolean;
+          runKit: boolean;
+        }
+      ) => {
+        let pageUrl: string = null;
 
+        console.log(options);
+
+        if (keyword && options.version) {
+          pageUrl = `https://www.npmjs.com/package/${keyword}?activeTab=versions`;
+        } else if (keyword && options.code) {
+          pageUrl = `https://www.npmjs.com/package/${keyword}?activeTab=code`;
+        } else if (keyword && options.dependencies) {
+          pageUrl = `https://www.npmjs.com/package/${keyword}?activeTab=dependencies`;
+        } else if (keyword && options.runKit) {
+          pageUrl = `https://npm.runkit.com/${keyword}`;
+        } else if (keyword) {
+          pageUrl = `https://www.npmjs.com/search?q=${keyword}`;
+        } else {
+          pageUrl = `https://www.npmjs.com`;
+        }
+
+        await open(pageUrl);
+      }
+    );
+
+  return command;
+}
 
 export { npmSearchCommand };
